45184a78a2524d90b62228376750505f1a6f3ee3
franmonopoli <54753485+franmonopoli@users.noreply.github.com>
2019-10-17 22:43:47 -0300

    Pre y post de anticuerpo

--- Anticuerpo.cpp
+++ Anticuerpo.cpp
@@ -1,10 +1,38 @@
-#include "Anticuerpo.h"
+#include "Elementos.h"
+#include "Celula.h"
+#ifndef ANTICUERPO_H_INCLUDED
+#define ANTICUERPO_H_INCLUDED
 
-anticuerpo::anticuerpo()
+class anticuerpo: public Elemento
 {
-    pos_x = 0;
-    pos_y = 0;
-}
 
-int anticuerpo::get_posX(){
-    return pos_x}
+public:
+        //CONSTRUCTORES
+        //SIN PARAMETROS
+        //Pre:-
+        //Post:Asigna 0 a posX e posY
+        anticuerpo();
+
+        //METODOS
+        //GETTERS
+        //POS X
+        //Pre:-
+        //Post: Devuelve la posicion en X que se encuentra el anticuerpo
+        int get_posX();
+        //POS Y
+        //Pre:-
+        //Post: Devuelve la posicion en Y que se encuentra el anticuerpo
+        int get_posY();
+
+        //SETTERS
+        //POS X
+        //Pre: La posicion pasada tiene que ser valida
+        //Post: Asigna el entero pasado a pos_X
+        void set_posX(int);
+        //POS Y
+        //Pre: La posicion pasada sea valida
+        //Post: Asigna el entero pasado a pos_Y
+        void set_posY(int);
+};
+
+#endif // ANTICUERPO_H_INCLUDED
