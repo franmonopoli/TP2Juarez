1794182c121b8c6590cf1582acfac2854d645af8
franmonopoli <54753485+franmonopoli@users.noreply.github.com>
2019-10-17 22:46:11 -0300

    Update Elementos.h

--- Elementos.h
+++ Elementos.h
@@ -1,6 +1,5 @@
-#ifndef ELEMENTOS_H_INCLUDED
-#define ELEMENTOS_H_INCLUDED
-
+#ifndef ELEMENTO_H
+#define ELEMENTO_H
 #include <string>
 #include <iostream>
 
@@ -10,15 +9,39 @@ using namespace std;
 class Elemento
 {
     public:
+        //CONSTRUCTORES
+        //SIN PARAMETROS
+        //Pre:-
+        //Post: Asigna "Graciela" a estado y 0 a ambas posiciones
         Elemento();
-        Elemento(float,float);
-        void get_posX();
-        void get_posY();
-        float set_posX(float);
-        float set_posY(float);
+        //CON PARAMETROS
+        //Pre: t tiene que ser un dato valido
+        //Post: Asigna t al atributo tipo y 0 a ambas posiciones
+        Elemento(string t);
+
+        //METODOS
+        //GETTERS
+        //Pre:-
+        //Post:Devuelve el tipo del elemento
+        string getTipo();
+        //MUESTREO
+        //Pre:-
+        //Post: Muestra el elemento
+        void muestreo();
+        //SET TIPO
+        //Pre: El tipo pasado tiene que ser valido
+        //Post: Asgina el tipo pasado al atributo tipo
+        void setTipo(string);
+        virtual ~Elemento();
+
     protected:
+        /**ATRIBUTOS EN PROTEGIDO**/
+        /* El atributo tipo lo heredan todos los elementos, de manera tal que al recorrer la lista de elementos
+        solo sea necesario consultar su tipo para indicarle al programa a que funciones debe llamar. */
+        string tipo;
         float posX;
         float posY;
+
 };
 
-#endif // ELEMENTOS_H_INCLUDED
+#endif // ELEMENTO_H
