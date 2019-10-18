e5cb654c17b643a4f80c5fe2c8abb3c259d33459
franmonopoli <54753485+franmonopoli@users.noreply.github.com>
2019-10-17 22:44:33 -0300

    Ahora esta bien xd

--- Anticuerpo.h
+++ Anticuerpo.h
@@ -7,10 +7,31 @@ class anticuerpo: public Elemento
 {
 
 public:
+        //CONSTRUCTORES
+        //SIN PARAMETROS
+        //Pre:-
+        //Post:Asigna 0 a posX e posY
         anticuerpo();
+
+        //METODOS
+        //GETTERS
+        //POS X
+        //Pre:-
+        //Post: Devuelve la posicion en X que se encuentra el anticuerpo
         int get_posX();
+        //POS Y
+        //Pre:-
+        //Post: Devuelve la posicion en Y que se encuentra el anticuerpo
         int get_posY();
+
+        //SETTERS
+        //POS X
+        //Pre: La posicion pasada tiene que ser valida
+        //Post: Asigna el entero pasado a pos_X
         void set_posX(int);
+        //POS Y
+        //Pre: La posicion pasada sea valida
+        //Post: Asigna el entero pasado a pos_Y
         void set_posY(int);
 };
 
